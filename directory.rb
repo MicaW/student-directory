d38f8ff06f0a60c4085bb881881bac226bbb063e
micawhitby <micawhitby@gmail.com>
2016-12-09 13:27:41 +0000

    Completed exercise 1

--- directory.rb
+++ directory.rb
@@ -1,22 +1,39 @@
 #lets put all students into an array
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
-#and print them
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+students = []
+#get the first name
+name = gets.chomp
+#while the name is not empty, repeat this code
+while !name.empty? do
+  #add the student hash to the array
+  students << {name: name, cohort: :november}
+  puts "Now we have #{students.count} students"
+  #get another name from the user
+  name = gets.chomp
+end
+#return the array of students
+students
+end
+
+def print_header
 puts "The students of Villains Academy"
 puts "--------------"
-students.each do |student|
-  puts student
 end
-#finally we print the total number of students
-print "Overall, we have #{students.count} great students"
+
+def print (students)
+  students.each_with_index do |student, index|
+  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print (students)
+print_footer(students)
