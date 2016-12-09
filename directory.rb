4d39ec76859a0a2c59ebda211bfae5b99249a81f
micawhitby <micawhitby@gmail.com>
2016-12-09 16:16:02 +0000

    Completed exercise 6

--- directory.rb
+++ directory.rb
@@ -25,12 +25,12 @@ end
 
 def print (students)
   students.each_with_index do |student, index|
-  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end
 end
 
 def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_students
