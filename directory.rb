1b6b14f026ebba2a39dc8d0c3fb4b1027a07427d
micawhitby <micawhitby@gmail.com>
2016-12-09 16:29:27 +0000

    Completed exercise 3

--- directory.rb
+++ directory.rb
@@ -25,7 +25,7 @@ end
 
 def print(students)
   students.each do |student|
-    if student[:name].start_with? ('d')
+    if student[:name].length < 12
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
