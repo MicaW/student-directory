ab53797affc8a65b26dddac532416f04d8f20b2b
micawhitby <micawhitby@gmail.com>
2016-12-10 14:17:11 +0000

    Completed exercise 5

--- directory.rb
+++ directory.rb
@@ -1,41 +1,62 @@
-#lets put all students into an array
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #create an empty array
-students = []
-#get the first name
-name = gets.chomp
-#while the name is not empty, repeat this code
-while !name.empty? do
-  #add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
-  #get another name from the user
-  name = gets.chomp
-end
-#return the array of students
-students
+
+=begin
+Our code only works with the student name and cohort.
+Add more information: hobbies, country of birth, height, etc.
+=end
+
+def print_input_students
+ puts "Please enter the names of the students"
+ puts "To finish, just hit return twice"
+ students = []
+ name = gets.chomp.capitalize
+ while !name.empty? do
+   students << { name: name, cohort: :november }
+   puts "Now we have #{students.count} students"
+   name = gets.chomp.capitalize
+ end
+ students
 end
 
 def print_header
-puts "The students of Villains Academy"
-puts "--------------"
+ puts "The students of Villains Academy"
+ puts "-------------"
 end
 
 def print(students)
-  students.each do |student|
-    if student[:name].length < 12
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
+ students.each_with_index { |n,i| puts "#{i+1}. #{n[:name]} (#{n[:cohort]} cohort)"}
 end
 
 def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+ puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
+def add_student_info
+ puts "Which student?"
+ students_name = gets.chomp
+ info = []
+ info << students_name
+ puts "Hobbies?"
+ hobbies = gets.chomp
+ info << hobbies
+ puts "Country of birth?"
+ cob = gets.chomp
+ info << cob
+ puts "Height?"
+ height = gets.chomp
+ info << height
+ puts "All done!"
+ puts "So, just to check I got all that:"
+ puts "#{info[0]}'s hobbies are #{info[1]}"
+ puts "#{info[0]}'s country of birth is #{info[2]}"
+ puts "#{info[0]} is #{info[3]} meters tall"
+end
+
+students = print_input_students
 print_header
-print (students)
+print(students)
 print_footer(students)
+puts "Do you want to add more information? (y/n)"
+ reply = gets.chomp
+ if reply == "y"
+   add_student_info
+ end
